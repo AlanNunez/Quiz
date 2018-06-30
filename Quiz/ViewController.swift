5f77c27a6d71dc7df90db3c2272c51b2733dcb15
Alan Nunez <nunear@icloud.com>
2018-06-30 04:51:01 -0400

    Swap Current Question With Next Question Label After Completion

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -37,10 +37,16 @@ class ViewController: UIViewController {
     }
     func animatedLabelTransitions()
     {
-            UIView.animate(withDuration: 0.5, animations: {() -> Void in
+        UIView.animate(withDuration: 0.5, delay:0, options: [],
+        animations:
+            {
                 self.currentQuestionLabel.alpha = 0
                 self.nextQuestionLabel.alpha = 1
-            })
+            }, completion:
+                {
+                    _ in swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                }
+        )
     }
     
     override func viewDidLoad() {
