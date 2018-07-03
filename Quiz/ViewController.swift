9f02796047f3e5e8f87c9791c143626b08b96070
Alan Nunez <nunear@icloud.com>
2018-07-03 02:46:45 -0400

    Fixed Animations Of Constraints

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
                              "Grapes"]
     var currentQuestionIndex: Int = 0
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet weak var answerLabel: UILabel!
     
     @IBAction func showNextQuestion(_ sender: UIButton) {
@@ -37,21 +39,35 @@ class ViewController: UIViewController {
     }
     func animatedLabelTransitions()
     {
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
         UIView.animate(withDuration: 0.5, delay:0, options: [],
         animations:
             {
                 self.currentQuestionLabel.alpha = 0
                 self.nextQuestionLabel.alpha = 1
+                
+                self.view.layoutIfNeeded()
             }, completion:
                 {
                     _ in swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
-                }
-        )
+                         swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                    self.updateOffScreenLabel()
+                })
+
+    }
+    func updateOffScreenLabel()
+    {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -61,5 +77,6 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         nextQuestionLabel.alpha = 0
     }
+
 }
 
